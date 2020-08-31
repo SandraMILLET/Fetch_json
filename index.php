85512608061b6ea7f5e9d158a6e13b4449075566
SandraMILLET <sandra.millet11@gmail.com>
2020-08-31 12:01:19 +0200

    indentation

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>JSON Joueur</title>
 </head>
+
 <body>
-<h1>Créer json, l'afficher en console puis dans la page web</h1>
-<h2>Le DOM se récupère-t-il?</h2>
+    <h1>Créer json, l'afficher en console puis dans la page web</h1>
+    <h2>Le DOM se récupère-t-il?</h2>
     <div id="joueur">
-    
+
     </div>
     <h2>
-    Présentation du joueur :
+        Présentation du joueur :
     </h2>
-<div id="result">
-</div>
+    <div id="result">
+    </div>
     <script src="js/script.js"></script>
 </body>
+
 </html>
